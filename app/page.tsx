45f89d5114a767f99eb1469fc56f885b122c5c78
marques-j-robinson <marques.j.robinson@gmail.com>
2024-01-26 04:22:30 -0800

    update site title, description and customize favicon

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,17 @@ import { usePathname } from "next/navigation";
 export default function Home() {
   const pathname = usePathname();
   return (
-    <header className="container mx-auto px-4 mt-8">
-        <nav className="flex flex-row items-center space-between justify-between">
-            <Link href="/"><h1 className="text-4xl font-bold">Landscapes by Marques</h1></Link>
-            <ul>
-                <li className={`inline-block px-4 uppercase ${pathname === "/" ? "active" : ""}`}><Link href="/">Gallery</Link></li>
-                <li className={`inline-block px-4 uppercase ${pathname === "/blog" ? "active" : ""}`}><Link href="/blog">Blog</Link></li>
-                <li className={`inline-block px-4 uppercase ${pathname === "/about" ? "active" : ""}`}><Link href="/about">About</Link></li>
-            </ul>
-        </nav>
-    </header>
+    <>
+        <header className="container mx-auto px-4 mt-8">
+            <nav className="flex flex-row items-center space-between justify-between">
+                <Link href="/"><h1 className="text-4xl font-bold">Landscapes by Marques</h1></Link>
+                <ul>
+                    <li className={`inline-block px-4 uppercase ${pathname === "/" ? "active" : ""}`}><Link href="/">Gallery</Link></li>
+                    <li className={`inline-block px-4 uppercase ${pathname === "/blog" ? "active" : ""}`}><Link href="/blog">Blog</Link></li>
+                    <li className={`inline-block px-4 uppercase ${pathname === "/about" ? "active" : ""}`}><Link href="/about">About</Link></li>
+                </ul>
+            </nav>
+        </header>
+    </>
   );
 }
